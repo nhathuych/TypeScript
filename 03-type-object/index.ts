b6b14302ff0f4e4fb1eb08afaece1ccaa7ece55a
Huy Huy <nhathuych@gmail.com>
2025-03-09 15:15:15 +0700

    object

---
+++ 03-type-object/index.ts
@@ -0,0 +1,9 @@
+let person: { name: string, age: number, isMale?: boolean } = {
+  name: 'Elon Musk',
+  age: 19,
+  // isMale is optional
+}
+
+// person.address = "USA" // property does not exist
+
+console.log(person)
